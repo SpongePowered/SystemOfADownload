d88fc3cf3fce9483541de42776d5065abf65d937
Gabriel Harris-Rouquette <gabizou@me.com>
2021-09-06 18:27:31 -0700

    Enable remaining services for full e2e testing
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -21,26 +21,26 @@ module "artifacts_server" {
     signature_key = local.signature_key
 }
 
-//module "artifacts_query_server" {
-//    depends_on = [
-//        kubernetes_job.soad-liquibase-migration,
-//        module.artifacts_server]
-//    source = "./lagom-base"
-//    environment = var.environment
-//    namespace = var.namespace
-//    app_name = local.soad_app_configs.artifacts_query.service_name
-//
-//    replica-count = local.soad_app_configs.artifacts_query.image.replicas
-//
-//    app_image = local.soad_app_configs.artifacts_query.image.image_name
-//    app_version = local.soad_app_configs.artifacts_query.image.image_version
-//
-//    extra_envs = local.soad_app_configs.artifacts_query.extra_env
-//    extra_secret_envs = local.soad_app_configs.artifacts_query.extra_secret_envs
-//
-//    encryption_key = local.encryption_key
-//    signature_key = local.signature_key
-//}
+module "artifacts_query_server" {
+    depends_on = [
+        kubernetes_job.soad-liquibase-migration,
+        module.artifacts_server]
+    source = "./lagom-base"
+    environment = var.environment
+    namespace = var.namespace
+    app_name = local.soad_app_configs.artifacts_query.service_name
+
+    replica-count = local.soad_app_configs.artifacts_query.image.replicas
+
+    app_image = local.soad_app_configs.artifacts_query.image.image_name
+    app_version = local.soad_app_configs.artifacts_query.image.image_version
+
+    extra_envs = local.soad_app_configs.artifacts_query.extra_env
+    extra_secret_envs = local.soad_app_configs.artifacts_query.extra_secret_envs
+
+    encryption_key = local.encryption_key
+    signature_key = local.signature_key
+}
 
 module "auth_server" {
     source = "./lagom-base"
@@ -68,6 +68,8 @@ module "versions" {
     app_name = local.soad_app_configs.versions.service_name
     replica-count = local.soad_app_configs.versions.replicas
     kafka_topics = local.soad_app_configs.versions.kafka_topics
+    extra_envs = local.soad_app_configs.versions.extra_env
+    extra_secret_envs = local.soad_app_configs.versions.extra_secret_envs
     namespace = var.namespace
     environment = var.environment
 
@@ -75,43 +77,43 @@ module "versions" {
     signature_key = local.signature_key
 }
 
-//module "versions_query" {
-//    depends_on = [
-//        kubernetes_job.soad-liquibase-migration,
-//        module.versions]
-//    source = "./lagom-base"
-//    app_image = local.soad_app_configs.versions_query.image.name
-//    app_version = local.soad_app_configs.versions_query.image.version
-//    app_name = local.soad_app_configs.versions_query.service_name
-//    replica-count = local.soad_app_configs.versions_query.replicas
-//    extra_envs = local.soad_app_configs.versions_query.extra_env
-//    extra_secret_envs = local.soad_app_configs.versions_query.extra_secret_env
-//    namespace = var.namespace
-//    environment = var.environment
-//    encryption_key = local.encryption_key
-//    signature_key = local.signature_key
-//}
+module "versions_query" {
+    depends_on = [
+        kubernetes_job.soad-liquibase-migration,
+        module.versions]
+    source = "./lagom-base"
+    app_image = local.soad_app_configs.versions_query.image.name
+    app_version = local.soad_app_configs.versions_query.image.version
+    app_name = local.soad_app_configs.versions_query.service_name
+    replica-count = local.soad_app_configs.versions_query.replicas
+    extra_envs = local.soad_app_configs.versions_query.extra_env
+    extra_secret_envs = local.soad_app_configs.versions_query.extra_secret_env
+    namespace = var.namespace
+    environment = var.environment
+    encryption_key = local.encryption_key
+    signature_key = local.signature_key
+}
 
-//module "versions_syncrhonizer" {
-//    depends_on = [
-//        kubernetes_job.soad-liquibase-migration,
-//        module.versions_query,
-//        module.versions,
-//        module.artifacts_query_server]
-//    source = "./lagom-base"
-//    namespace = var.namespace
-//    environment = var.environment
-//
-//    app_image = local.soad_app_configs.synchronizer.image.name
-//    app_version = local.soad_app_configs.synchronizer.image.version
-//    app_name = local.soad_app_configs.synchronizer.service_name
-//    replica-count = local.soad_app_configs.synchronizer.replicas
-//    extra_envs = local.soad_app_configs.synchronizer.extra_env
-//    extra_secret_envs = local.soad_app_configs.synchronizer.extra_secret_env
-//    extra_ports = local.soad_app_configs.synchronizer.extra_ports
-//    encryption_key = local.encryption_key
-//    signature_key = local.signature_key
-//}
+module "versions_syncrhonizer" {
+    depends_on = [
+        kubernetes_job.soad-liquibase-migration,
+        module.versions_query,
+        module.versions,
+        module.artifacts_query_server]
+    source = "./lagom-base"
+    namespace = var.namespace
+    environment = var.environment
+
+    app_image = local.soad_app_configs.synchronizer.image.name
+    app_version = local.soad_app_configs.synchronizer.image.version
+    app_name = local.soad_app_configs.synchronizer.service_name
+    replica-count = local.soad_app_configs.synchronizer.replicas
+    extra_envs = local.soad_app_configs.synchronizer.extra_env
+    extra_secret_envs = local.soad_app_configs.synchronizer.extra_secret_env
+    extra_ports = local.soad_app_configs.synchronizer.extra_ports
+    encryption_key = local.encryption_key
+    signature_key = local.signature_key
+}
 
 //module "gateway" {
 //    // currently disable gateway from being deployed
